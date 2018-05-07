e48f391486baf0f00cc708caf86c0f374f73307f
Yulong Wu <yulong@nuco.io>
2018-05-07 14:12:01 -0400

    Update test case

--- org.aion.avm.rt/test/org/aion/avm/classloader/App.java
+++ org.aion.avm.rt/test/org/aion/avm/classloader/App.java
@@ -1,6 +1,15 @@
 package org.aion.avm.classloader;
 
 public class App {
+
+    public int foo;
+
+    public static int bar;
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
