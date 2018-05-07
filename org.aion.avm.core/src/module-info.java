fd291d60d4c1b6655a5e0643dea94a5c1e2189ff
Roman Katerinenko <roman@nuco.io>
2018-05-07 15:45:32 -0400

    Fix dependencies

--- org.aion.avm.core/src/module-info.java
+++ org.aion.avm.core/src/module-info.java
@@ -2,9 +2,11 @@ module org.aion.avm.core {
     exports org.aion.avm.core;
     exports org.aion.avm.core.impl;
     // external modules
-//    requires asm;
-//    requires asm.commons;
-//    requires asm.tree;
-//    requires asm.util;
     requires slf4j.api;
+    requires junit;
+    requires hamcrest.all;
+    requires org.objectweb.asm;
+    requires org.objectweb.asm.commons;
+    requires org.objectweb.asm.tree;
+    requires org.objectweb.asm.util;
 }
