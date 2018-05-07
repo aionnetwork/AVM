e7d9d71374396f7232bc5da2b29c47ca708b7c4b
Roman Katerinenko <roman@nuco.io>
2018-05-07 14:05:01 -0400

    Replace checked exception with unchecked one

--- org.aion.avm.core/src/org/aion/avm/core/impl/AvmImpl.java
+++ org.aion.avm.core/src/org/aion/avm/core/impl/AvmImpl.java
@@ -22,25 +22,25 @@ public class AvmImpl implements Avm {
         loadContract(contractModulesPath, startModuleName, fullyQualifiedMainClassName);
     }
 
-    private void loadContract(String contractModulesPath, String startModuleName, String fullyQualifiedMainClassName) throws Exception {
+    private void loadContract(String contractModulesPath, String startModuleName, String fullyQualifiedMainClassName) {
         final var bootLayer = ModuleLayer.boot();
         final var contractModulesFinder = ModuleFinder.of(Paths.get(contractModulesPath));
         final var emptyFinder = ModuleFinder.of();
         final var contractLayerConfig = bootLayer.configuration().resolve(contractModulesFinder, emptyFinder, List.of(startModuleName));
         final var contractLayer = bootLayer.defineModulesWithOneLoader(contractLayerConfig, avmClassLoader);
-        final var contractModuleClassLoader = contractLayer.findModule(startModuleName).orElseThrow(() -> new Exception("Module not found")).getClassLoader();
-        mainContractClass = contractModuleClassLoader.loadClass(fullyQualifiedMainClassName);
+        try {
+            mainContractClass = contractLayer.findModule(startModuleName)
+                    .orElseThrow(() -> new Exception("Module not found"))
+                    .getClassLoader()
+                    .loadClass(fullyQualifiedMainClassName);
+        } catch (Exception e) {
+            final var msg = format("Unable to load contract. Start module:'%s', main class:'%s', module path:'%s'",
+                    startModuleName, fullyQualifiedMainClassName, contractModulesPath);
+            throw new IllegalStateException(msg, e);
+        }
     }
 
     public Class getMainContractClass() {
         return mainContractClass;
-    }
-
-    public static class ContractLoadingException extends Exception {
-
-        public ContractLoadingException(String contractModulesPath, String startModuleName, String mainClass) {
-            super(format("Unable to load contract. Start module:'%s', main class:'%s', module path:'%s'", startModuleName, mainClass, contractModulesPath));
-        }
-
     }
 }
