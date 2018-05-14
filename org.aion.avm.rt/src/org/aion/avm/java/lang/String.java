6c19737ea02d3ef11eff1cf3032034d4d0cead63
Jeff Disher <jeffdisher@nuco.io>
2018-05-14 14:25:47 -0400

    Created minimal testing functionality of java/lang shadow package
    -this will be used by a forthcoming changed - carved out on its own to make the later change more focussed

--- org.aion.avm.rt/src/org/aion/avm/java/lang/String.java
+++ org.aion.avm.rt/src/org/aion/avm/java/lang/String.java
@@ -1,5 +1,15 @@
 package org.aion.avm.java.lang;
 
 public class String extends Object {
+    private final java.lang.String underlying;
+    
+    public String(java.lang.String underlying) {
+        this.underlying = underlying;
+    }
 
+    // NOTE:  This toString() cannot be called by the contract code (it will call avm_toString()) but our runtime and test code can call this.
+    @Override
+    public java.lang.String toString() {
+        return this.underlying.toString();
+    }
 }
