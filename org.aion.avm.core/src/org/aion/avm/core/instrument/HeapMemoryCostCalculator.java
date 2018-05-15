ee5ee008a7df53bb6d3d6c04ced9fdbac8d0c9f6
nancyaion <nancy@nuco.io>
2018-05-15 13:30:28 -0400

    update HeapMemoryCostCalculator code

--- org.aion.avm.core/src/org/aion/avm/core/instrument/HeapMemoryCostCalculator.java
+++ org.aion.avm.core/src/org/aion/avm/core/instrument/HeapMemoryCostCalculator.java
@@ -1,10 +1,13 @@
 package org.aion.avm.core.instrument;
 
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+
 import java.util.HashMap;
 
 /**
  * Heap memory is allocated at the new object creation. This class provides a map of every class' instance size.
- * At every time an object is created by the "new" instruction, a piece of heap memory of this size is allocated.
+ * Every time an object is created by the "new" instruction, a piece of heap memory of this size is allocated.
  * The accordingly memory usage cost is then charged on the Energy meter.
  *
  * The hashmap stores the instance size of every classes, including,
@@ -24,7 +27,7 @@ public class HeapMemoryCostCalculator {
      * Enum - class field size based on the descriptor / type.
      * Size in bits.
      */
-    public enum FieldTypeSize {
+    public enum FieldTypeSizeInBits {
         BYTE        (8),
         CHAR        (16),
         SHORT       (16),
@@ -37,7 +40,7 @@ public class HeapMemoryCostCalculator {
 
         private final long val;
 
-        FieldTypeSize(long val) {
+        FieldTypeSizeInBits(long val) {
             this.val = val;
         }
 
@@ -46,11 +49,16 @@ public class HeapMemoryCostCalculator {
         }
     }
 
-    private class HeapMemoryInfo {
+    /**
+     * A data structure that stores the heap size and the fields info of one class.
+     * To decide the heap size, the derived class need to compare its declared fields to the inherited fields that are
+     * from the parent class. For this purpose, all the declared and inherited fields are recorded in this data struct.
+     */
+    public class HeapMemoryInfo {
         /**
          * The instance size of a class. In bytes.
          */
-        long heapSize = 0; // in bytes
+        private long heapSize = 0; // in bytes
 
         /**
          * A Hashmap that stores the class' fields, including the inherited ones.
@@ -65,11 +73,18 @@ public class HeapMemoryCostCalculator {
          */
         private HashMap<String, String> fieldsMap;
 
+        /**
+         * Constructor.
+         */
         private HeapMemoryInfo() {
             fieldsMap = new HashMap<>();
         }
 
-        private void setHeapSize(long heapSize) {
+        /**
+         * Set the heapSize value of the class.
+         * @param heapSize the heapSize value.
+         */
+        public void setHeapSize(long heapSize) {
             if (heapSize >= 0) {
                 this.heapSize = heapSize;
             }
@@ -78,12 +93,31 @@ public class HeapMemoryCostCalculator {
             }
         }
 
-        // size can be positive or negative.
-        private void modifyHeapSize(long size) {
-            this.heapSize += size;
+        /**
+         * Modify the heapSize by adding it with the input size. This method is used during the calculation of heapSize.
+         * @param size the size to modify with. It can be positive or negative.
+         */
+        public void modifyHeapSize(long size) {
+            heapSize += size;
+            if (heapSize < 0) {
+                throw new IllegalStateException("The heapSize cannot be negative. Calculation is wrong.");
+            }
         }
 
-        private void addToFieldsMap(String fieldName, String fieldType) {
+        /**
+         * Get the heapSize value of the class.
+         * @return the heapSize of the class.
+         */
+        public long getHeapSize() {
+            return heapSize;
+        }
+
+        /**
+         * Add one field to the fields list of the class.
+         * @param fieldName A String of the field's name.
+         * @param fieldType A String of the field's descriptor.
+         */
+        public void addToFieldsMap(String fieldName, String fieldType) {
             if (fieldsMap == null) {
                 throw new IllegalStateException("HeapMemoryInfo does not have the fieldsMap.");
             }
@@ -95,29 +129,41 @@ public class HeapMemoryCostCalculator {
 
     /**
      * A hashmap that stores the instance size of every classes.
+     * Key - class name
+     * Value - the heapMemoryInfo of the class
      */
-    private HashMap<String, HeapMemoryInfo> classInstanceSizeMap;
+    private HashMap<String, HeapMemoryInfo> classHeapInfoMap;
 
+    /**
+     * Constructor
+     */
     public HeapMemoryCostCalculator() {
-        classInstanceSizeMap = new HashMap<>();
+        classHeapInfoMap = new HashMap<>();
     }
 
     /**
-     * Calculate the instance size of one class and record it in the "classInstanceSizeMap", as long as its fields.
+     * Calculate the instance size of one class and record it in the "classHeapInfoMap", as long as its fields.
      * @param classBytes input class bytecode stream.
      *
      * Note, this method is called from the top to bottom of the class inheritance hierarchy tree. Such that it can
-     * be assumed that the parent classes' heapMemoryInfo is already in the hashmap.
+     * be assumed that the parent classes' heapMemoryInfo is already in the map.
      */
     public void calcInstanceSizeOfOneClass(byte[] classBytes) {
-        if (classInstanceSizeMap == null) {
-            throw new IllegalStateException("HeapMemoryCostCalculator does not have the classInstanceSizeMap.");
+        if (classHeapInfoMap == null) {
+            throw new IllegalStateException("HeapMemoryCostCalculator does not have the classHeapInfoMap.");
         }
 
-        // read the class name; check if it is already in the classInstanceSizeMap
+        // read in, build the classNode
+        ClassNode classNode = new ClassNode();
+        ClassReader cr = new ClassReader(classBytes);
+        cr.accept(classNode, 0);
 
-        // calculate it if not in the classInstanceSizeMap
+        // read the class name; check if it is already in the classHeapInfoMap
+        if (classHeapInfoMap.containsKey(classNode.name)) {
+            return;
+        }
 
+        // calculate it if not in the classHeapInfoMap
         HeapMemoryInfo heapMemoryInfo = new HeapMemoryInfo();
 
         // get the parent classes, combine the fields and copy to heapMemoryInfo.fieldsMap (check the hiding, dups, etc)
@@ -127,5 +173,36 @@ public class HeapMemoryCostCalculator {
 
     }
 
+    /**
+     * Calculate the instance sizes of classes and record them in the "classHeapInfoMap", as long as their fields.
+     *
+     * This method is applied with all the classes that may be instantiated during the execution of a smart contract,
+     * 1. when JVM starts, apply this method to the java.lang.* classes;
+     * 2. when runtime starts, apply this method to the runtime classes;
+     * 3. at the deployment of a smart contract, apply this method to all the classes of the contract.
+     */
+    public void calcClassesInstanceSize() {
+
+
+        InheritanceHierarchyBuilder.buildHierarchy();
+
+        // pop from the stack and call calcInstanceSizeOfOneClass
+        byte[] classBytes = null;
+        calcInstanceSizeOfOneClass(classBytes);
+    }
+
+
+    /**
+     * A helper class that builds the hierarchy tree of the classes.
+     */
+    private static class InheritanceHierarchyBuilder {
+
+        /**
+         * build a stack of classes, in which the top ones should not be derived from the bottom ones.
+         */
+        private static void buildHierarchy() {
+
+        }
+    }
 
 }
