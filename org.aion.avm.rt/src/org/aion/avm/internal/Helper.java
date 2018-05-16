5d686d7083199ce1a62c44b030cab7b7e8d33564
Yulong Wu <yulong@nuco.io>
2018-05-16 17:33:23 -0400

    Store blockchain runtime in Helper

--- org.aion.avm.rt/src/org/aion/avm/internal/Helper.java
+++ org.aion.avm.rt/src/org/aion/avm/internal/Helper.java
@@ -1,6 +1,14 @@
 package org.aion.avm.internal;
 
+import org.aion.avm.rt.BlockchainRuntime;
+
 public class Helper {
+
+    private static ThreadLocal<BlockchainRuntime> blockchainRuntime = new ThreadLocal<>();
+
+    public static void setBlockchainRuntime(BlockchainRuntime rt) {
+        blockchainRuntime.set(rt);
+    }
 
     public static <T> org.aion.avm.java.lang.Class<T> wrapAsClass(Class<T> input) {
         return new org.aion.avm.java.lang.Class<T>(input);
