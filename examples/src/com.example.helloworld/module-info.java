f88f460f6091ba3ea952195c41c0dddca212c388
Yulong Wu <yulong@nuco.io>
2018-05-18 12:34:52 -0400

    Re #37: add examples folder

---
+++ examples/src/com.example.helloworld/module-info.java
@@ -0,0 +1,5 @@
+module org.aion.avm.core.examples {
+    exports com.example.helloworld;
+
+    requires org.aion.avm.rt;
+}
---
+++ examples/src/com.example.simpletoken/module-info.java
@@ -0,0 +1,5 @@
+module org.aion.avm.core.examples {
+    exports com.example.simpletoken;
+
+    requires org.aion.avm.rt;
+}
