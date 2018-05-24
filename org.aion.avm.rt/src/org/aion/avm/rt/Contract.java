e7bf3d03fc2cc73655df3c36ab05ec451514e8d0
nancyaion <nancy@nuco.io>
2018-05-24 13:44:40 -0400

    Contract interface has 2 methods now, 'contractCreation' and 'run'

--- org.aion.avm.rt/src/org/aion/avm/rt/Contract.java
+++ org.aion.avm.rt/src/org/aion/avm/rt/Contract.java
@@ -4,7 +4,19 @@ import org.aion.avm.arraywrapper.ByteArray;
 
 public interface Contract {
     /**
-     * Executes this start smart with the given input.
+     * Entry point of execution at deployment with the given input.
+     *
+     * This method is executed only once at the deployment. It takes the parameters that are transmitted in the transaction data field.
+     * Normally, the user implements this method to initialize the state of the contract and put it into the storage. The runtime provides the APIs to access the storage.
+     *
+     * @param input the input
+     * @param rt    the runtime
+     * @return the output
+     */
+    ByteArray contractCreation(ByteArray input, BlockchainRuntime rt);
+
+    /**
+     * Entry point of execution at every call to this smart contract with the given input.
      *
      * @param input the input
      * @param rt    the runtime
