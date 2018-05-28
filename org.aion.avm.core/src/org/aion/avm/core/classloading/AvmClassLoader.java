bd1656115be7bf09a32551babdf194f2eb58da59
Jeff Disher <jeffdisher@nuco.io>
2018-05-28 15:51:14 -0400

    Re #55: Added a cache, removed the whitelist, and overrode the more appropriate loadClass method
    -note that the whitelist was removed since we now expect our re-writing to give us the appropriate isolation (failing that, we would need the whitelist to know about the special cases we inject, like the assumption that the real "java/lang/Throwable" is accessible)
    -this is moving us to the point where this can fully replace TestClassLoader

--- org.aion.avm.core/src/org/aion/avm/core/classloading/AvmClassLoader.java
+++ org.aion.avm.core/src/org/aion/avm/core/classloading/AvmClassLoader.java
@@ -3,20 +3,19 @@ package org.aion.avm.core.classloading;
 import java.util.*;
 import java.util.function.Function;
 
+
+/**
+ * NOTE:  This implementation assumes that the classes we are trying to load are "safe" in that they don't reference
+ * anything we don't want this classloader to load.
+ * While we originally imposed some of our isolation at the classloader level, we now assume that is done in the
+ * bytecode instrumentation/analysis phase.
+ */
 public class AvmClassLoader extends ClassLoader {
-
-    private static final Set<String> WHITELISTED_PACKAGES = new HashSet<>();
-
-    static {
-        WHITELISTED_PACKAGES.add("org.aion.avm.arraywrapper.");
-        WHITELISTED_PACKAGES.add("org.aion.avm.exceptionwrapper.");
-        WHITELISTED_PACKAGES.add("org.aion.avm.internal.");
-        WHITELISTED_PACKAGES.add("org.aion.avm.java.lang.");
-        WHITELISTED_PACKAGES.add("org.aion.avm.rt.");
-    }
-
     private Map<String, byte[]> classes;
     private List<Function<String, byte[]>> handlers;
+
+    // Since we are using our own loadClass, we need our own cache.
+    private final Map<String, Class<?>> cache;
 
     /**
      * Constructs a new AVM class loader.
@@ -27,6 +26,7 @@ public class AvmClassLoader extends ClassLoader {
     public AvmClassLoader(Map<String, byte[]> classes, List<Function<String, byte[]>> handlers) {
         this.classes = classes;
         this.handlers = handlers;
+        this.cache = new HashMap<>();
     }
 
     public AvmClassLoader(Map<String, byte[]> classes) {
@@ -34,32 +34,49 @@ public class AvmClassLoader extends ClassLoader {
     }
 
     @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException {
-
-        System.out.println(name);
-
-        if (WHITELISTED_PACKAGES.stream().anyMatch(k -> name.startsWith(k))) {
-
-            // runtime classes
-            return super.loadClass(name);
-
-        } else if (classes.containsKey(name)) {
-
-            // user-defined classes
-            byte[] bytecode = classes.get(name);
-            return defineClass(name, bytecode, 0, bytecode.length);
-
+    public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        // NOTE:  We override this, instead of findClass, since we want to circumvent the normal delegation process of class loaders.
+        Class<?> result = null;
+        boolean shouldResolve = resolve;
+        
+        // We have a priority order to load:
+        // 1) Cache
+        // 2) Injected static code
+        // 3) Dynamically generated
+        // 4) Parent
+        if (this.cache.containsKey(name)) {
+            result = this.cache.get(name);
+            // We got this from the cache so don't resolve.
+            shouldResolve = false;
+        } else if (this.classes.containsKey(name)) {
+            byte[] injected = this.classes.get(name);
+            result = defineClass(name, injected, 0, injected.length);
+            this.cache.put(name, result);
         } else {
-
-            // dynamically generated classes
+            // Before falling back to the parent, try the dynamic.
             for (Function<String, byte[]> handler : handlers) {
                 byte[] code = handler.apply(name);
                 if (code != null) {
-                    return defineClass(name, code, 0, code.length);
+                    result = defineClass(name, code, 0, code.length);
+                    this.cache.put(name, result);
+                    break;
                 }
             }
-
+            
+            // If all else fails, the parent.
+            if (null == result) {
+                result = getParent().loadClass(name);
+                // We got this from the parent so don't resolve.
+                shouldResolve = false;
+            }
+        }
+        
+        if ((null != result) && shouldResolve) {
+            resolveClass(result);
+        }
+        if (null == result) {
             throw new ClassNotFoundException();
         }
+        return result;
     }
 }
