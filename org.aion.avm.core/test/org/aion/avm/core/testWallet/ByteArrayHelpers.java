b0fcb52329a6aa2099c923be23e3693dae6c5082
Jeff Disher <jeffdisher@nuco.io>
2018-06-11 13:59:18 -0400

    Re #59 Remove dependency on java.nio.ByteBuffer and System.arraycopy
    -this dependency restriction is required in order to eventually run this on Avm

--- org.aion.avm.core/test/org/aion/avm/core/testWallet/ByteArrayHelpers.java
+++ org.aion.avm.core/test/org/aion/avm/core/testWallet/ByteArrayHelpers.java
@@ -1,20 +1,32 @@
 package org.aion.avm.core.testWallet;
 
-import java.nio.ByteBuffer;
-
 
 /**
  * Helpers for manipulating byte[] in high-level ways.
  */
 public class ByteArrayHelpers {
     public static byte[] appendLong(byte[] array, long l) {
-        // Just use a ByteBuffer to serialize the long.
-        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
-        buffer.putLong(l);
-        byte[] longBytes = buffer.array();
+        // We want to avoid any special JDK dependencies, so crack the long into big-endian bytes (network order).
+        byte[] longBytes = new byte[] {
+                (byte) (0xff & (l >> 56)),
+                (byte) (0xff & (l >> 48)),
+                (byte) (0xff & (l >> 40)),
+                (byte) (0xff & (l >> 32)),
+                (byte) (0xff & (l >> 24)),
+                (byte) (0xff & (l >> 16)),
+                (byte) (0xff & (l >>  8)),
+                (byte) (0xff & (l >>  0)),
+        };
         byte[] newArray = new byte[array.length + longBytes.length];
-        System.arraycopy(array, 0, newArray, 0, array.length);
-        System.arraycopy(longBytes, 0, newArray, array.length, longBytes.length);
+        arraycopy(array, 0, newArray, 0, array.length);
+        arraycopy(longBytes, 0, newArray, array.length, longBytes.length);
         return newArray;
+    }
+
+    public static void arraycopy(byte[] src, int srcPos, byte[] dest, int destPos, int length) {
+        for (int i = 0; i < length; ++i) {
+            byte elt = src[srcPos + i];
+            dest[destPos + i] = elt;
+        }
     }
 }
