b74c71d9e777cc007b79ec0d53906d4aa9f83cd1
JunhanHu-aion <junhan@nuco.io>
2018-06-19 15:27:26 -0400

    Re #105: Shadow java.lang.Number implementation and examination.

--- org.aion.avm.rt/src/org/aion/avm/java/lang/Number.java
+++ org.aion.avm.rt/src/org/aion/avm/java/lang/Number.java
@@ -1,4 +1,19 @@
 package org.aion.avm.java.lang;
 
-public class Number extends Object {
+public abstract class Number extends Object {
+    public abstract int avm_intValue();
+
+    public abstract long avm_longValue();
+
+    public abstract float avm_floatValue();
+
+    public abstract double avm_doubleValue();
+
+    public byte avm_byteValue() {
+        return (byte)avm_intValue();
+    }
+
+    public short avm_shortValue() {
+        return (short)avm_intValue();
+    }
 }
