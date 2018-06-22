28ebfaf020b9efad76b668758386ec792d67802a
JunhanHu-aion <junhan@nuco.io>
2018-06-22 11:31:16 -0400

    Re #133: Shadow StringBuffer StringBuilder

---
+++ org.aion.avm.rt/src/org/aion/avm/shadow/java/lang/StringBuffer.java
@@ -0,0 +1,9 @@
+package org.aion.avm.shadow.java.lang;
+
+public class StringBuffer {
+    private java.lang.StringBuffer underlying;
+
+    public java.lang.StringBuffer getUnderlying() {
+        return underlying;
+    }
+}
---
+++ org.aion.avm.rt/src/org/aion/avm/shadow/java/lang/StringBuilder.java
@@ -0,0 +1,9 @@
+package org.aion.avm.shadow.java.lang;
+
+public class StringBuilder {
+    private java.lang.StringBuilder underlying;
+
+    public java.lang.StringBuilder getUnderlying() {
+        return underlying;
+    }
+}
