918fdab7dfe21c0d4b9a95439550d0a9e6418bf8
Yulong Wu <yulong@nuco.io>
2018-07-09 14:45:53 -0400

    Move the byte array wrapper

---
+++ org.aion.avm.core/src/org/aion/avm/core/util/ByteArrayWrapper.java
@@ -0,0 +1,38 @@
+package org.aion.avm.core.util;
+
+import org.aion.kernel.TransformedDappStorage;
+
+import java.util.Arrays;
+
+/**
+ * A wrapper of byte[], to be used as the HashMap key.
+ */
+public final class ByteArrayWrapper
+{
+    private final byte[] data;
+
+    public ByteArrayWrapper(byte[] data)
+    {
+        if (data == null)
+        {
+            throw new NullPointerException();
+        }
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        if (!(object instanceof ByteArrayWrapper))
+        {
+            return false;
+        }
+        return Arrays.equals(data, ((ByteArrayWrapper)object).data);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Arrays.hashCode(data);
+    }
+}
