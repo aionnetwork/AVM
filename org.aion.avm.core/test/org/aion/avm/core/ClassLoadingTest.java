6d34097d560b195c3cd3450f9acd6bc67a24ce7c
Yulong Wu <yulong@nuco.io>
2018-07-23 10:07:50 -0400

    Add class loading CPU contention test

---
+++ org.aion.avm.core/test/org/aion/avm/core/ClassLoadingTest.java
@@ -0,0 +1,40 @@
+package org.aion.avm.core;
+
+import org.aion.avm.core.util.Helpers;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ClassLoadingTest {
+
+    private String clazzName = ClassLoadingResource.class.getName();
+    private byte[] clazzBytes = Helpers.loadRequiredResourceAsBytes(clazzName.replaceAll("\\.", "/") + ".class");
+
+    @Test
+    @Ignore
+    public void test() throws InterruptedException {
+
+        for (int i = 0; i < 4; i++) {
+            new Thread(() -> {
+                while (true) {
+                    ClassLoader cl = new ClassLoader() {
+                        @Override
+                        public Class<?> loadClass(String name) throws ClassNotFoundException {
+                            if (name.equals(clazzName)) {
+                                return super.defineClass(name, clazzBytes, 0, clazzBytes.length);
+                            } else {
+                                return super.loadClass(name);
+                            }
+                        }
+                    };
+                    try {
+                        cl.loadClass(clazzName);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+        Thread.sleep(30 * 1000);
+    }
+}
