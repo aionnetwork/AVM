bb72d093aa0d9f9f5130ec7b760def239803dd4b
nancyaion <nancy@nuco.io>
2018-07-25 16:17:11 -0400

    ant script to copy over ERC20 test code to examples/

--- examples/src/com.example.testExchange/com/example/testExchange/ERC20Token.java
+++ examples/src/com.example.testExchange/com/example/testExchange/ERC20Token.java
@@ -3,7 +3,9 @@ package com.example.testExchange;
 import org.aion.avm.api.Address;
 import org.aion.avm.api.BlockchainRuntime;
 
+
 public class ERC20Token implements ERC20 {
+
     public final String name;
     public final String symbol;
     public final int decimals;
@@ -16,7 +18,7 @@ public class ERC20Token implements ERC20 {
 
     private long totalSupply;
 
-    public ERC20Token(String name, String symbol, int decimals, Address minter){
+    public ERC20Token(String name, String symbol, int decimals, Address minter) {
         this.name = name;
         this.symbol = symbol;
         this.decimals = decimals;
@@ -37,53 +39,53 @@ public class ERC20Token implements ERC20 {
         return decimals;
     }
 
-    public long totalSupply(){
+    public long totalSupply() {
         return totalSupply;
     }
 
-    public long balanceOf(Address tokenOwner){
+    public long balanceOf(Address tokenOwner) {
         return this.ledger.getOrDefault(tokenOwner, 0L);
     }
 
-    public long allowance(Address tokenOwner, Address spender){
-        if (!this.allowance.containsKey(tokenOwner)){
+    public long allowance(Address tokenOwner, Address spender) {
+        if (!this.allowance.containsKey(tokenOwner)) {
             return 0L;
         }
 
         return this.allowance.get(tokenOwner).getOrDefault(spender, 0L);
     }
 
-    public boolean transfer(Address receiver, long tokens){
+    public boolean transfer(Address receiver, long tokens) {
         Address sender = BlockchainRuntime.getSender();
 
         long senderBalance = this.ledger.getOrDefault(sender, 0L);
         long receiverBalance = this.ledger.getOrDefault(receiver, 0L);
 
-        if ((senderBalance >= tokens) && (tokens > 0) && (receiverBalance + tokens > 0)){
-            // TODO: emit Transfer event
+        if ((senderBalance >= tokens) && (tokens > 0) && (receiverBalance + tokens > 0)) {
             this.ledger.put(sender, senderBalance - tokens);
             this.ledger.put(receiver, receiverBalance + tokens);
+            BlockchainRuntime.log("Transfer".getBytes(), sender.unwrap(), receiver.unwrap(), Long.toString(tokens).getBytes());
             return true;
         }
 
         return false;
     }
 
-    public boolean approve(Address spender, long tokens){
+    public boolean approve(Address spender, long tokens) {
         Address sender = BlockchainRuntime.getSender();
 
-        if (!this.allowance.containsKey(sender)){
+        if (!this.allowance.containsKey(sender)) {
             AionMap<Address, Long> newEntry = new AionMap<>();
             this.allowance.put(sender, newEntry);
         }
 
-        // TODO: emit Approval event
+        BlockchainRuntime.log("Approval".getBytes(), sender.unwrap(), spender.unwrap(), Long.toString(tokens).getBytes());
         this.allowance.get(sender).put(spender, tokens);
 
         return true;
     }
 
-    public boolean transferFrom(Address from, Address to, long tokens){
+    public boolean transferFrom(Address from, Address to, long tokens) {
         Address sender = BlockchainRuntime.getSender();
 
         long fromBalance = this.ledger.getOrDefault(from, 0L);
@@ -91,8 +93,8 @@ public class ERC20Token implements ERC20 {
 
         long limit = allowance(from, sender);
 
-        if ((fromBalance > tokens) && (limit > tokens) && (toBalance + tokens > 0)){
-            // TODO: emit Transfer event
+        if ((fromBalance > tokens) && (limit > tokens) && (toBalance + tokens > 0)) {
+            BlockchainRuntime.log("Transfer".getBytes(), from.unwrap(), to.unwrap(), Long.toString(tokens).getBytes());
             this.ledger.put(from, fromBalance - tokens);
             this.allowance.get(from).put(sender, limit - tokens);
             this.ledger.put(to, toBalance + tokens);
@@ -102,11 +104,11 @@ public class ERC20Token implements ERC20 {
         return false;
     }
 
-    public boolean mint(Address receiver, long tokens){
-        if (BlockchainRuntime.getSender().equals(this.minter)){
+    public boolean mint(Address receiver, long tokens) {
+        if (BlockchainRuntime.getSender().equals(this.minter)) {
             long receiverBalance = this.ledger.getOrDefault(receiver, 0L);
-            if ((tokens > 0) && (receiverBalance + tokens > 0)){
-                // TODO: emit Mint event
+            if ((tokens > 0) && (receiverBalance + tokens > 0)) {
+                BlockchainRuntime.log("Mint".getBytes(), receiver.unwrap(), Long.toString(tokens).getBytes());
                 this.ledger.put(receiver, receiverBalance + tokens);
                 this.totalSupply += tokens;
                 return true;
