0fb7cd74a35605dd107cff8d0f5ed6c818ffd3b9
JunhanHu-aion <junhan@nuco.io>
2018-08-10 17:27:48 -0400

    Fix #164:Shared class loader now cache class refs from its parents.
    Shared class loader now use a per class lock instead of a global lock for class loading request.

--- org.aion.avm.core/src/org/aion/avm/core/classloading/AvmSharedClassLoader.java
+++ org.aion.avm.core/src/org/aion/avm/core/classloading/AvmSharedClassLoader.java
@@ -4,8 +4,8 @@ import org.aion.avm.core.arraywrapping.ArrayWrappingClassGenerator;
 import org.aion.avm.core.util.Assert;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 
@@ -14,66 +14,74 @@ import java.util.function.Function;
  * as part of the contract namespace, but is common and class-immutable across all contracts.
  */
 public class AvmSharedClassLoader extends ClassLoader {
-    private Map<String, byte[]> classes;
+    // Pre generated shadow JCL classes
+    private final Map<String, byte[]> classes;
 
     // Since we are using our own loadClass, we need our own cache.
     private final Map<String, Class<?>> cache;
 
+    // List of class generator entry points
     private ArrayList<Function<String, byte[]>> handlers;
 
     public AvmSharedClassLoader(Map<String, byte[]> classes) {
         this.classes = classes;
-        this.cache = new HashMap<>();
-
+        this.cache = new ConcurrentHashMap<>();
         this.handlers = new ArrayList<>();
+
         registerHandlers();
     }
 
+    // Register runtime class generator
     private void registerHandlers(){
         Function<String, byte[]> wrapperGenerator = (cName) -> ArrayWrappingClassGenerator.arrayWrappingFactory(cName, this);
         this.handlers.add(wrapperGenerator);
     }
 
     @Override
-    synchronized public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+    public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
         // NOTE:  We override this, instead of findClass, since we want to circumvent the normal delegation process of class loaders.
         Class<?> result = null;
         boolean shouldResolve = resolve;
-        
-        if (this.cache.containsKey(name)) {
-            result = this.cache.get(name);
-            // We got this from the cache so don't resolve.
-            shouldResolve = false;
-        } else if (this.classes.containsKey(name)) {
-            byte[] injected = this.classes.get(name);
-            result = defineClass(name, injected, 0, injected.length);
-            this.cache.put(name, result);
-        } else {
-            // All user space class should be loaded with contract loader
-            Assert.assertTrue(!name.contains("org.aion.avm.user"));
 
-            // Before falling back to the parent, try the dynamic.
-            for (Function<String, byte[]> handler : handlers) {
-                byte[] code = handler.apply(name);
-                if (code != null) {
-                    result = defineClass(name, code, 0, code.length);
+        // Acquire per class class loading lock
+        synchronized (this.getClassLoadingLock(name)) {
 
-                    Assert.assertTrue(!this.cache.containsKey(name));
-                    this.cache.put(name, result);
-                    break;
+            if (this.cache.containsKey(name)) {
+                result = this.cache.get(name);
+                // We got this from the cache so don't resolve.
+                shouldResolve = false;
+            } else if (this.classes.containsKey(name)) {
+                byte[] injected = this.classes.get(name);
+                result = defineClass(name, injected, 0, injected.length);
+                this.cache.putIfAbsent(name, result);
+            } else {
+                // All user space class should be loaded with contract loader
+                Assert.assertTrue(!name.contains("org.aion.avm.user"));
+
+                // Before falling back to the parent, try the dynamic.
+                for (Function<String, byte[]> handler : handlers) {
+                    byte[] code = handler.apply(name);
+                    if (code != null) {
+                        result = defineClass(name, code, 0, code.length);
+                        Assert.assertTrue(!this.cache.containsKey(name));
+                        this.cache.putIfAbsent(name, result);
+                        break;
+                    }
+                }
+
+                if (null == result) {
+                    result = getParent().loadClass(name);
+                    // We got this from the parent so don't resolve.
+                    shouldResolve = false;
+                    this.cache.putIfAbsent(name, result);
                 }
             }
 
-            if (null == result) {
-                result = getParent().loadClass(name);
-                // We got this from the parent so don't resolve.
-                shouldResolve = false;
+            if (null != result && shouldResolve) {
+                resolveClass(result);
             }
         }
-        
-        if ((null != result) && shouldResolve) {
-            resolveClass(result);
-        }
+
         return result;
     }
 }
