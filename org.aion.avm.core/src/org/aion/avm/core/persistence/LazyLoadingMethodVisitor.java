72000cc03bcbd0a80aa94157e3cb66869db46aed
Jeff Disher <jeffdisher@nuco.io>
2018-08-15 16:18:14 -0400

    Re #156: Added bytecode index accounting
    -this is just to split this commit from the more complex one which actually starts making decisions depending on this accounting

--- org.aion.avm.core/src/org/aion/avm/core/persistence/LazyLoadingMethodVisitor.java
+++ org.aion.avm.core/src/org/aion/avm/core/persistence/LazyLoadingMethodVisitor.java
@@ -2,6 +2,8 @@ package org.aion.avm.core.persistence;
 
 import org.aion.avm.core.util.DescriptorParser;
 import org.aion.avm.core.util.Helpers;
+import org.objectweb.asm.Handle;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -26,12 +28,120 @@ public class LazyLoadingMethodVisitor extends MethodVisitor {
     private static final String LAZY_LOAD_NAME = "lazyLoad";
     private static final String LAZY_LOAD_DESCRIPTOR = "()V";
 
+    // TODO:  Start using this with the completion of issue-156 (currently just added to get the big and benign part of the change into its own commit).
+    @SuppressWarnings("unused")
+    private int frameOffset;
+
     public LazyLoadingMethodVisitor(MethodVisitor visitor) {
         super(Opcodes.ASM6, visitor);
     }
 
     @Override
     public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
+        checkInjectLazyLoad(opcode, descriptor);
+        super.visitFieldInsn(opcode, owner, name, descriptor);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitInsn(int opcode) {
+        super.visitInsn(opcode);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitIntInsn(int opcode, int operand) {
+        super.visitIntInsn(opcode, operand);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitVarInsn(int opcode, int var) {
+        super.visitVarInsn(opcode, var);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitTypeInsn(int opcode, String type) {
+        super.visitTypeInsn(opcode, type);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
+        super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitInvokeDynamicInsn(String name, String descriptor, Handle bootstrapMethodHandle, Object... bootstrapMethodArguments) {
+        super.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitJumpInsn(int opcode, Label label) {
+        super.visitJumpInsn(opcode, label);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitLdcInsn(Object value) {
+        super.visitLdcInsn(value);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitIincInsn(int var, int increment) {
+        super.visitIincInsn(var, increment);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitTableSwitchInsn(int min, int max, Label dflt, Label... labels) {
+        super.visitTableSwitchInsn(min, max, dflt, labels);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
+        super.visitLookupSwitchInsn(dflt, keys, labels);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitMultiANewArrayInsn(String descriptor, int numDimensions) {
+        super.visitMultiANewArrayInsn(descriptor, numDimensions);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitFrame(int type, int nLocal, Object[] local, int nStack, Object[] stack) {
+        super.visitFrame(type, nLocal, local, nStack, stack);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitLabel(Label label) {
+        super.visitLabel(label);
+        this.frameOffset += 1;
+    }
+
+    @Override
+    public void visitLineNumber(int line, Label start) {
+        super.visitLineNumber(line, start);
+        this.frameOffset += 1;
+    }
+
+
+    /**
+     * NOTE:  All calls to instruction visitation routines are made against super, directly, since we do frame offset accounting within our overrides
+     * and that offset only applies to incoming bytecodes, not outgoing ones.
+     * 
+     * @param opcode The opcode.
+     * @param descriptor The type descriptor of the field to which the opcode is applied.
+     */
+    private void checkInjectLazyLoad(int opcode, String descriptor) {
         // If this is a PUTFIELD or GETFIELD, we want to call "lazyLoad()":
         // -PUTIFELD:  DUP2, POP, INVOKEVIRTUAL
         // -GETIFELD:  DUP, INVOKEVIRTUAL
@@ -42,34 +152,33 @@ public class LazyLoadingMethodVisitor extends MethodVisitor {
                 // Where we need:  ... OBJECT, VAR1, VAR2, OBJECT (top)
                 // This is multiple stages:
                 // DUP2_X1: ... VAR1, VAR2, OBJECT, VAR1, VAR2 (top)
-                this.visitInsn(Opcodes.DUP2_X1);
+                super.visitInsn(Opcodes.DUP2_X1);
                 // POP2: ... VAR1, VAR2, OBJECT (top)
-                this.visitInsn(Opcodes.POP2);
+                super.visitInsn(Opcodes.POP2);
                 // DUP: ... VAR1, VAR2, OBJECT, OBJECT (top)
-                this.visitInsn(Opcodes.DUP);
+                super.visitInsn(Opcodes.DUP);
                 // INOKE: ... VAR1, VAR2, OBJECT (top)
-                this.visitMethodInsn(Opcodes.INVOKEVIRTUAL, SHADOW_OBJECT_NAME, LAZY_LOAD_NAME, LAZY_LOAD_DESCRIPTOR, false);
+                super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, SHADOW_OBJECT_NAME, LAZY_LOAD_NAME, LAZY_LOAD_DESCRIPTOR, false);
                 // DUP_X2: ... OBJECT, VAR1, VAR2, OBJECT (top)
-                this.visitInsn(Opcodes.DUP_X2);
+                super.visitInsn(Opcodes.DUP_X2);
                 // POP: ... OBJECT, VAR1, VAR2 (top)
-                this.visitInsn(Opcodes.POP);
+                super.visitInsn(Opcodes.POP);
             } else {
                 // Here, the stack looks like: ... OBJECT, VAR, (top)
                 // Where we need:  ... OBJECT, VAR, OBJECT (top)
                 // Stages:
                 // DUP2: ... OBJECT, VAR, OBJECT, VAR (top)
-                this.visitInsn(Opcodes.DUP2);
+                super.visitInsn(Opcodes.DUP2);
                 // POP: ... OBJECT, VAR, OBJECT (top)
-                this.visitInsn(Opcodes.POP);
+                super.visitInsn(Opcodes.POP);
                 // INOKE: ... OBJECT, VAR (top)
-                this.visitMethodInsn(Opcodes.INVOKEVIRTUAL, SHADOW_OBJECT_NAME, LAZY_LOAD_NAME, LAZY_LOAD_DESCRIPTOR, false);
+                super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, SHADOW_OBJECT_NAME, LAZY_LOAD_NAME, LAZY_LOAD_DESCRIPTOR, false);
             }
         } else if (Opcodes.GETFIELD == opcode) {
             // Here, the stack looks like: ... OBJECT, (top)
             // Where we need:  ... OBJECT, OBJECT (top)
-            this.visitInsn(Opcodes.DUP);
-            this.visitMethodInsn(Opcodes.INVOKEVIRTUAL, SHADOW_OBJECT_NAME, LAZY_LOAD_NAME, LAZY_LOAD_DESCRIPTOR, false);
+            super.visitInsn(Opcodes.DUP);
+            super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, SHADOW_OBJECT_NAME, LAZY_LOAD_NAME, LAZY_LOAD_DESCRIPTOR, false);
         }
-        super.visitFieldInsn(opcode, owner, name, descriptor);
     }
 }
