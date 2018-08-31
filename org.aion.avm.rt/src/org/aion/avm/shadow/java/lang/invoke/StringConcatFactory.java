6085ab93baa8c271d9dc95608e7ad8967f16a042
romaion <roman@aion.network>
2018-08-31 13:35:00 -0400

    #213 Adding a unit test for concatenation with constants

--- org.aion.avm.rt/src/org/aion/avm/shadow/java/lang/invoke/StringConcatFactory.java
+++ org.aion.avm.rt/src/org/aion/avm/shadow/java/lang/invoke/StringConcatFactory.java
@@ -3,10 +3,7 @@ package org.aion.avm.shadow.java.lang.invoke;
 import org.aion.avm.internal.IHelper;
 import org.aion.avm.shadow.java.lang.String;
 
-import java.lang.invoke.ConstantCallSite;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodType;
-import java.lang.invoke.StringConcatException;
+import java.lang.invoke.*;
 import java.util.Arrays;
 
 
@@ -30,7 +27,9 @@ public final class StringConcatFactory extends org.aion.avm.shadow.java.lang.Obj
     }
 
     /**
-     * A bootstrap method for handling string concatination
+     * A bootstrap method for handling string concatenation
+     *
+     * @see java.lang.invoke.StringConcatFactory#makeConcatWithConstants(MethodHandles.Lookup, java.lang.String, MethodType, java.lang.String, Object...)
      */
     public static java.lang.invoke.CallSite avm_makeConcatWithConstants(
             java.lang.invoke.MethodHandles.Lookup owner,
@@ -51,40 +50,9 @@ public final class StringConcatFactory extends org.aion.avm.shadow.java.lang.Obj
                 .asType(invokedType);
         return new ConstantCallSite(concatMethodHandle);
     }
-//    public static org.aion.avm.core.testdoubles.indy.String concat(java.lang.String recipe, // for future use
-//                                                                   java.lang.Object[] constants, // for future use
-//                                                                   org.aion.avm.core.testdoubles.indy.String[] strings) {
-//        final var builder = new StringBuilder();
-//        Arrays.stream(strings)
-//                .map(org.aion.avm.core.testdoubles.indy.String::getOriginal)
-//                .forEach(builder::append);
-//        return new org.aion.avm.core.testdoubles.indy.String(builder.toString());
-//    }
-//
-//    /**
-//     * A bootstrap method for handling string concatination
-//     */
-//    public static java.lang.invoke.CallSite avm_makeConcatWithConstants(
-//            java.lang.invoke.MethodHandles.Lookup owner,
-//            java.lang.String invokedName,
-//            java.lang.invoke.MethodType invokedType,
-//            java.lang.String recipe,
-//            java.lang.Object... constants) throws StringConcatException, NoSuchMethodException, IllegalAccessException {
-//        final var concatMethodType = MethodType.methodType(
-//                org.aion.avm.core.testdoubles.indy.String.class, // NOTE! First arg is return value
-//                String.class,
-//                Object[].class,
-//                org.aion.avm.core.testdoubles.indy.String[].class);
-//        final MethodHandle concatMethodHandle = owner
-//                .findStatic(StringConcatFactory.class, "concat", concatMethodType)
-//                .bindTo(recipe)
-//                .bindTo(constants)
-//                .asVarargsCollector(org.aion.avm.core.testdoubles.indy.String[].class)
-//                .asType(invokedType);
-//        return new ConstantCallSite(concatMethodHandle);
-//    }
 
     // Cannot be instantiated.
-    private StringConcatFactory() {}
+    private StringConcatFactory() {
+    }
     // Note:  No instances can be created so no deserialization constructor required.
 }
