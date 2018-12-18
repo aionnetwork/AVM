eae3bbc5411a8288cfe1fb7b6c837c841abb8776
aionick <nick@nuco.io>
2018-12-18 18:23:14 -0500

    InternalTransaction implements API interface
    - The API interface this class now implements is
      InternalTransactionInterface.

--- org.aion.avm.core/src/org/aion/avm/core/types/InternalTransaction.java
+++ org.aion.avm.core/src/org/aion/avm/core/types/InternalTransaction.java
@@ -2,11 +2,12 @@ package org.aion.avm.core.types;
 
 import java.math.BigInteger;
 import org.aion.kernel.Transaction;
+import org.aion.vm.api.interfaces.InternalTransactionInterface;
 
 /**
  * Represents an AION internal transaction
  */
-public class InternalTransaction extends Transaction {
+public class InternalTransaction extends Transaction implements InternalTransactionInterface {
 
     private boolean rejected ;
 
@@ -19,11 +20,41 @@ public class InternalTransaction extends Transaction {
         return 0;
     }
 
+    @Override
     public void markAsRejected() {
         this.rejected = true;
     }
 
+    @Override
     public boolean isRejected() {
         return rejected;
     }
+
+    @Override
+    public byte[] getParentTransactionHash() {
+        throw new AssertionError("No corresponding Avm concept exists for this yet.");
+    }
+
+    //TODO: This is currently implemented in TransactionContextImpl and needs to be refactored into
+    //TODO: this class instead.
+    @Override
+    public int getStackDepth() {
+        throw new AssertionError("No corresponding Avm concept exists for this yet.");
+    }
+
+    @Override
+    public int getIndexOfInternalTransaction() {
+        throw new AssertionError("No corresponding Avm concept exists for this yet.");
+    }
+
+    @Override
+    public String getNote() {
+        throw new AssertionError("No corresponding Avm concept exists for this yet.");
+    }
+
+    @Override
+    public byte[] getEncoded() {
+        throw new AssertionError("No corresponding Avm concept exists for this yet.");
+    }
+
 }
