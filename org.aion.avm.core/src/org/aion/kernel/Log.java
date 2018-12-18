60fdf9866e4edf0e667fcdb6434a1dab12e7f1ab
aionick <nick@nuco.io>
2018-12-18 17:19:23 -0500

    Log implements API IExecutionLog
    - This class likely will also be extracted out into a common util
      library.
    - Currently the bloom filter representing the log uses the blake2b
      hash function, which is the kernel's default. We still need to
      confirm that the blake2b is always used, or get to understand what
      conditions may apply.

--- org.aion.avm.core/src/org/aion/kernel/Log.java
+++ org.aion.avm.core/src/org/aion/kernel/Log.java
@@ -1,14 +1,16 @@
 package org.aion.kernel;
 
+import org.aion.avm.core.util.HashUtils;
 import org.aion.avm.core.util.Helpers;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.aion.vm.api.interfaces.IExecutionLog;
 
 /**
  * Represents a log emitted by dapp.
  */
-public class Log {
+public class Log implements IExecutionLog {
     private byte[] address;
     private List<byte[]> topics;
     private byte[] data;
@@ -19,6 +21,11 @@ public class Log {
         this.data = data;
     }
 
+    @Override
+    public AvmAddress getSourceAddress() {
+        return AvmAddress.wrap(this.address);
+    }
+
     public byte[] getAddress() {
         return address;
     }
@@ -27,6 +34,7 @@ public class Log {
         this.address = address;
     }
 
+    @Override
     public List<byte[]> getTopics() {
         return topics;
     }
@@ -35,12 +43,41 @@ public class Log {
         this.topics = topics;
     }
 
+    @Override
     public byte[] getData() {
         return data;
     }
 
     public void setData(byte[] data) {
         this.data = data;
+    }
+
+    /**
+     * The kernel implementation of this method actually uses a hash function that can be set, although
+     * blake2b is the default and it doesn't seem there is any exposed way of altering this. This
+     * has to be confirmed, though this type will likely get pushed up into a common util place
+     * where this decision will be made.
+     *
+     * @return A {@link BloomFilter} representing this log.
+     */
+    @Override
+    public BloomFilter getBloomFilterForLog() {
+        BloomFilter filter = new BloomFilter(HashUtils.blake2b(this.address));
+        for (byte[] topic : this.topics) {
+            filter.or(new BloomFilter(HashUtils.blake2b(topic)));
+        }
+        return filter;
+    }
+
+    /**
+     * Not to be used.
+     *
+     * Eventually this type will get pulled out into a common space, where a reliable serialization
+     * can be put in place so that each project agrees upon it.
+     */
+    @Override
+    public byte[] getEncoded() {
+        return null;
     }
 
     @Override
