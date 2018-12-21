5f3d73068de4217f89fd6267dcf5a4076b7ce56e
Jeff Disher <jeffdisher@nuco.io>
2018-12-20 23:51:16 -0500

    Added SLF4J module requirements to satisfy an Eclipse compile error
    -not sure if this is Eclipse being very strict about module-info uses or something we actually _should_ be doing, which we work around due to the way we build our tests with Ant

--- org.aion.avm.rt/src/module-info.java
+++ org.aion.avm.rt/src/module-info.java
@@ -9,4 +9,9 @@ module org.aion.avm.rt {
     exports org.aion.avm.shadow.java.util.function;
     exports org.aion.avm.api;
     exports org.aion.avm;
+
+    // These are required in order to satisfy test compilation in Eclipse.
+    // (our build process may avoid this need in other cases - not sure if that is more/less correct)
+    requires slf4j.api;
+    requires slf4j.simple;
 }
--- org.aion.avm.userlib/src/module-info.java
+++ org.aion.avm.userlib/src/module-info.java
@@ -1,3 +1,8 @@
 module org.aion.avm.userlib {
     exports org.aion.avm.userlib;
+
+    // These are required in order to satisfy test compilation in Eclipse.
+    // (our build process may avoid this need in other cases - not sure if that is more/less correct)
+    requires slf4j.api;
+    requires slf4j.simple;
 }
