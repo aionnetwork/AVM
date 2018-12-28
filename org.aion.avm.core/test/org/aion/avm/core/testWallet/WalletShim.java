d84d650caa74cbc1f0ba505c16e6475d88ad2594
Jeff Disher <jeffdisher@nuco.io>
2018-12-28 18:11:51 -0500

    Re #330: Added a non-adjusted version of the WalletShim methods
    -the new ABI will use these entry-points (since it knows the difference between external/internal spaces)
    -this change isn't enabled until the next commit but this is safe to pull out in order to keep that large commit focused

--- org.aion.avm.core/test/org/aion/avm/core/testWallet/WalletShim.java
+++ org.aion.avm.core/test/org/aion/avm/core/testWallet/WalletShim.java
@@ -17,40 +17,80 @@ public class WalletShim {
         return ABIDecoder.decodeAndRunWithClass(WalletShim.class, input);
     }
 
+    public static void initWrapper(Address extra1, Address extra2, int requiredVotes, long dailyLimit) {
+        Wallet.initWrapper(extra1, extra2, requiredVotes, dailyLimit);
+    }
+
     public static void avm_initWrapper(Address extra1, Address extra2, int requiredVotes, long dailyLimit) {
         Wallet.initWrapper(extra1, extra2, requiredVotes, dailyLimit);
+    }
+
+    public static void payable(Address from, long value) {
+        Wallet.payable(from, value);
     }
 
     public static void avm_payable(Address from, long value) {
         Wallet.payable(from, value);
     }
 
+    public static boolean addOwner(Address owner) {
+        return Wallet.addOwner(owner);
+    }
+
     public static boolean avm_addOwner(Address owner) {
         return Wallet.addOwner(owner);
+    }
+
+    public static byte[] execute(Address to, long value, byte[] data) {
+        return Wallet.execute(to, value, data);
     }
 
     public static byte[] avm_execute(Address to, long value, ByteArray data) {
         return Wallet.execute(to, value, data.getUnderlying());
     }
 
+    public static boolean confirm(byte[] h) {
+        return Wallet.confirm(h);
+    }
+
     public static boolean avm_confirm(ByteArray h) {
         return Wallet.confirm(h.getUnderlying());
+    }
+
+    public static boolean changeRequirement(int newRequired) {
+        return Wallet.changeRequirement(newRequired);
     }
 
     public static boolean avm_changeRequirement(int newRequired) {
         return Wallet.changeRequirement(newRequired);
     }
 
+    public static Address getOwner(int ownerIndex) {
+        return Wallet.getOwner(ownerIndex);
+    }
+
     public static Address avm_getOwner(int ownerIndex) {
         return Wallet.getOwner(ownerIndex);
+    }
+
+    public static boolean changeOwner(Address from, Address to) {
+        return Wallet.changeOwner(from, to);
     }
 
     public static boolean avm_changeOwner(Address from, Address to) {
         return Wallet.changeOwner(from, to);
     }
 
+    public static boolean removeOwner(Address owner) {
+        return Wallet.removeOwner(owner);
+    }
+
     public static boolean avm_removeOwner(Address owner) {
         return Wallet.removeOwner(owner);
+    }
+
+    public static void revoke(byte[] transactionBytes) {
+        Wallet.revoke(transactionBytes);
     }
 
     public static void avm_revoke(ByteArray transactionBytes) {
