7d124ce54d92d019d5e2b7e121b33a5e4ff74d60
Zhike Zhang <zhike@aion.network>
2019-01-07 16:19:24 -0500

    Re #230: added batched version test
    One batched transaction context contains a lot of transactions.

--- org.aion.avm.core/test/org/aion/avm/core/performance/PerformanceTest.java
+++ org.aion.avm.core/test/org/aion/avm/core/performance/PerformanceTest.java
@@ -14,7 +14,9 @@ import org.aion.kernel.Block;
 import org.aion.kernel.KernelInterfaceImpl;
 import org.aion.kernel.Transaction;
 import org.aion.kernel.TransactionContextImpl;
+import org.aion.vm.api.interfaces.SimpleFuture;
 import org.aion.vm.api.interfaces.TransactionContext;
+import org.aion.vm.api.interfaces.TransactionResult;
 import org.aion.vm.api.interfaces.VirtualMachine;
 import org.junit.After;
 import org.junit.Assert;
@@ -26,9 +28,14 @@ public class PerformanceTest {
     private VirtualMachine avm;
     Block block;
 
-    private static final int userDappNum = 30;
+    private static final int transactionBlockSize = 10;
+    private static final int contextNum = 3;
+    // We want to use the same number for single and batched calls.
+    private static final int userDappNum = transactionBlockSize * contextNum;
     private static final int heavyLevel = 1;
     private static final int allocSize = (1 * (1 << 20));
+    private static long energyLimit = 1_000_000_000_000_000l;
+    private static long energyPrice = 1l;
 
     private org.aion.vm.api.interfaces.Address[] userAddrs = new org.aion.vm.api.interfaces.Address[userDappNum];
     private Address[] contractAddrs = new Address[userDappNum];
@@ -53,8 +60,6 @@ public class PerformanceTest {
         byte[] txData = new CodeAndArguments(jar, args).encodeToBytes();
 
         // Deploy
-        long energyLimit = 1_000_000l;
-        long energyPrice = 1l;
         for(int i = 0; i < userDappNum; ++i) {
             //creating users
             org.aion.vm.api.interfaces.Address userAddress = Helpers.randomAddress();
@@ -81,63 +86,93 @@ public class PerformanceTest {
     }
 
     @Test
-    public void testPerformanceCpuNto1() throws Exception {
-        long startTime = System.currentTimeMillis();
-
-        for (int i = 0; i < userDappNum; ++i) {
-            call(userAddrs[i], block, contractAddrs[0], "cpuHeavy");
-        }
-
-        long endTime = System.currentTimeMillis();
-        long timeElapsed = endTime - startTime;
-        System.out.printf("testPerformanceCpuNto1: %d ms\n", timeElapsed);
+    public void testPerformanceCpuNto1Single() throws Exception {
+        performanceTestSingle("cpuHeavy", true, "testPerformanceCpuNto1Single");
     }
 
     @Test
-    public void testPerformanceCpuNtoN() throws Exception {
-        long startTime = System.currentTimeMillis();
-
-        for (int i = 0; i < userDappNum; ++i) {
-            call(userAddrs[i], block, contractAddrs[i], "cpuHeavy");
-        }
-
-        long endTime = System.currentTimeMillis();
-        long timeElapsed = endTime - startTime;
-        System.out.printf("testPerformanceCpuNtoN: %d ms\n", timeElapsed);
+    public void testPerformanceCpuNtoNSingle() throws Exception {
+        performanceTestSingle("cpuHeavy", false, "testPerformanceCpuNtoNSingle");
     }
 
     @Test
-    public void testPerformanceMemoryNto1() throws Exception {
-        long startTime = System.currentTimeMillis();
-
-        for (int i = 0; i < userDappNum; ++i) {
-            call(userAddrs[i], block, contractAddrs[0], "memoryHeavy");
-        }
-
-        long endTime = System.currentTimeMillis();
-        long timeElapsed = endTime - startTime;
-        System.out.printf("testPerformanceMemoryNto1: %d ms\n", timeElapsed);
+    public void testPerformanceMemoryNto1Single() throws Exception {
+        performanceTestSingle("memoryHeavy", true, "testPerformanceMemoryNto1Single");
     }
 
     @Test
-    public void testPerformanceMemoryNtoN() throws Exception {
+    public void testPerformanceMemoryNtoNSingle() throws Exception {
+        performanceTestSingle("memoryHeavy", false, "testPerformanceMemoryNtoNSingle");
+    }
+
+    public void performanceTestSingle(String methodName, boolean Nto1, String testName) {
         long startTime = System.currentTimeMillis();
 
         for (int i = 0; i < userDappNum; ++i) {
-            call(userAddrs[i], block, contractAddrs[0], "memoryHeavy");
+            callSingle(userAddrs[i], block, Nto1 ? contractAddrs[0] : contractAddrs[i], methodName);
         }
 
         long endTime = System.currentTimeMillis();
         long timeElapsed = endTime - startTime;
-        System.out.printf("testPerformanceMemoryNtoN: %d ms\n", timeElapsed);
+        System.out.printf("%s: %d ms\n", testName, timeElapsed);
     }
 
-    private void call(org.aion.vm.api.interfaces.Address sender, Block block, Address contractAddr, String methodName) {
-        long energyLimit = 1_000_000_000_000_000l;
-        long energyPrice = 1l;
+    private void callSingle(org.aion.vm.api.interfaces.Address sender, Block block, Address contractAddr, String methodName) {
         byte[] argData = ABIEncoder.encodeMethodArguments(methodName);
         Transaction call = Transaction.call(sender, AvmAddress.wrap(contractAddr.unwrap()), kernel.getNonce(sender).longValue(), BigInteger.ZERO, argData, energyLimit, energyPrice);
         AvmTransactionResult result = (AvmTransactionResult) avm.run(new TransactionContext[] {new TransactionContextImpl(call, block)})[0].get();
         Assert.assertEquals(AvmTransactionResult.Code.SUCCESS, result.getResultCode());
+    }
+
+    /**
+     * One transaction context contains a lot of transactions.
+     */
+    @Test
+    public void testPerformanceCpuNto1Batch() throws Exception {
+        performanceBatch("cpuHeavy", true, "testPerformanceCpuNto1Batch");
+    }
+
+    @Test
+    public void testPerformanceCpuNtoNBatch() throws Exception {
+        performanceBatch("cpuHeavy",false, "testPerformanceCpuNtoNBatch");
+    }
+
+    @Test
+    public void testPerformanceMemoryNto1Batch() throws Exception {
+        performanceBatch("memoryHeavy", true, "testPerformanceMemoryNto1Batch");
+    }
+
+    @Test
+    public void testPerformanceMemoryNtoNBatch() throws Exception {
+        performanceBatch("memoryHeavy", false, "testPerformanceMemoryNtoNBatch");
+    }
+
+    public void performanceBatch(String methodName, boolean Nto1, String testName) {
+        long startTime = System.currentTimeMillis();
+
+        callBatch(methodName, block, Nto1);
+
+        long endTime = System.currentTimeMillis();
+        long timeElapsed = endTime - startTime;
+        System.out.printf("%s: %d ms\n", testName, timeElapsed);
+    }
+
+    public void callBatch(String methodName, Block block, boolean Nto1){
+        byte[] argData = ABIEncoder.encodeMethodArguments(methodName);
+        for(int j = 0; j < contextNum; ++j) {
+            TransactionContext[] transactionContext = new TransactionContext[transactionBlockSize];
+            for (int i = 0; i < transactionBlockSize; ++i) {
+                org.aion.vm.api.interfaces.Address sender = userAddrs[i];
+                Address contractAddr = Nto1 ? contractAddrs[0] : contractAddrs[i];
+                Transaction call = Transaction.call(sender, AvmAddress.wrap(contractAddr.unwrap()), kernel.getNonce(sender).longValue(), BigInteger.ZERO, argData, energyLimit, energyPrice);
+                transactionContext[i] = new TransactionContextImpl(call, block);
+            }
+            SimpleFuture<TransactionResult>[] futures = avm.run(transactionContext);
+            for (SimpleFuture<TransactionResult> future : futures) {
+                AvmTransactionResult result = (AvmTransactionResult) future.get();
+                Assert.assertEquals(AvmTransactionResult.Code.SUCCESS, result.getResultCode());
+                TestingHelper.decodeResult(result);
+            }
+        }
     }
 }
