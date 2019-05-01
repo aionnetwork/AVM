d513b2d6f50e773afa5986038904bd361cd6df01
Aayush Rajasekaran <arajasek94@gmail.com>
2019-05-01 11:48:23 -0400

    Remove unimplemented functionality from InternalTransaction, update the vm-api lib

--- org.aion.avm.core/src/org/aion/avm/core/types/InternalTransaction.java
+++ org.aion.avm.core/src/org/aion/avm/core/types/InternalTransaction.java
@@ -31,30 +31,4 @@ public class InternalTransaction extends Transaction implements InternalTransact
         return rejected;
     }
 
-    @Override
-    public byte[] getParentTransactionHash() {
-        throw new AssertionError("No corresponding Avm concept exists for this yet.");
-    }
-
-    //TODO (AKI-115): This is currently implemented in TransactionContextImpl and needs to be refactored into this class instead.
-    @Override
-    public int getStackDepth() {
-        throw new AssertionError("No corresponding Avm concept exists for this yet.");
-    }
-
-    @Override
-    public int getIndexOfInternalTransaction() {
-        throw new AssertionError("No corresponding Avm concept exists for this yet.");
-    }
-
-    @Override
-    public String getNote() {
-        throw new AssertionError("No corresponding Avm concept exists for this yet.");
-    }
-
-    @Override
-    public byte[] getEncoded() {
-        throw new AssertionError("No corresponding Avm concept exists for this yet.");
-    }
-
 }
