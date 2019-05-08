20c0056389fac546bef590a1e5f8bdb0e73fc504
aionick <nick@nuco.io>
2019-05-08 14:44:08 -0400

    Added some tests on nested lambdas
    - Msotly to verify that they get serialized and deserialized correctly.

---
+++ org.aion.avm.tooling/test/org/aion/avm/tooling/NestLambdaSerializationTest.java
@@ -0,0 +1,39 @@
+package org.aion.avm.tooling;
+
+import static org.junit.Assert.assertEquals;
+
+import avm.Address;
+import java.math.BigInteger;
+import org.aion.avm.core.util.ABIUtil;
+import org.aion.kernel.AvmTransactionResult.Code;
+import org.aion.vm.api.interfaces.TransactionResult;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class NestLambdaSerializationTest {
+    @Rule
+    public AvmRule avmRule = new AvmRule(false);
+
+    private Address deployer = avmRule.getPreminedAccount();
+
+    @Test
+    public void testSerializeDeserializeNestedLambda() {
+        byte[] jar = avmRule.getDappBytes(NestedLambdaTarget.class, new byte[0]);
+        TransactionResult result = avmRule.deploy(deployer, BigInteger.ZERO, jar, 5_000_000, 1).getTransactionResult();
+        assertEquals(Code.SUCCESS, result.getResultCode());
+        Address contract = new Address(result.getReturnData());
+
+        // Create the nested lambda.
+        avmRule.kernel.generateBlock();
+        byte[] data = ABIUtil.encodeMethodArguments("createLambda");
+        result = avmRule.call(deployer, contract, BigInteger.ZERO, data, 2_000_000, 1).getTransactionResult();
+        assertEquals(Code.SUCCESS, result.getResultCode());
+
+        // Invoke the nested lambda to verify it is created properly.
+        avmRule.kernel.generateBlock();
+        data = ABIUtil.encodeMethodArguments("callLambda");
+        result = avmRule.call(deployer, contract, BigInteger.ZERO, data, 2_000_000, 1).getTransactionResult();
+        assertEquals(Code.SUCCESS, result.getResultCode());
+    }
+
+}
