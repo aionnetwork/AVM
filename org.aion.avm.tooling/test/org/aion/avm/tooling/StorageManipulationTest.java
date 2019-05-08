3fb86164085b6b62e8c6845eaf28e9ddcce0931c
aionick <nick@nuco.io>
2019-05-08 14:05:32 -0400

    Added getStorage/putStorage manipulation tests
    - These tests attempt to modify the key/value byte arrays we place in
      the storage via getStorage and putStorage.

---
+++ org.aion.avm.tooling/test/org/aion/avm/tooling/StorageManipulationTest.java
@@ -0,0 +1,78 @@
+package org.aion.avm.tooling;
+
+import static org.junit.Assert.assertEquals;
+
+import avm.Address;
+import java.math.BigInteger;
+import org.aion.avm.core.util.ABIUtil;
+import org.aion.kernel.AvmTransactionResult.Code;
+import org.aion.vm.api.interfaces.TransactionResult;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class StorageManipulationTest {
+    @Rule
+    public AvmRule avmRule = new AvmRule(false);
+
+    private Address deployer = avmRule.getPreminedAccount();
+
+    @Test
+    public void testManipulatePutStorageKey() {
+        byte[] jar = avmRule.getDappBytes(StorageManipulationTarget.class, new byte[0]);
+        TransactionResult result = avmRule.deploy(deployer, BigInteger.ZERO, jar, 5_000_000, 1).getTransactionResult();
+        assertEquals(Code.SUCCESS, result.getResultCode());
+
+        avmRule.kernel.generateBlock();
+        Address contract = new Address(result.getReturnData());;
+        byte[] data = ABIUtil.encodeMethodArguments("manipulatePutStorageKey");
+        result = avmRule.call(deployer, contract, BigInteger.ZERO, data, 2_000_000, 1).getTransactionResult();
+
+        //TODO: this should be SUCCESS
+        assertEquals(Code.FAILED_REVERT, result.getResultCode());
+    }
+
+    @Test
+    public void testManipulatePutStorageValue() {
+        byte[] jar = avmRule.getDappBytes(StorageManipulationTarget.class, new byte[0]);
+        TransactionResult result = avmRule.deploy(deployer, BigInteger.ZERO, jar, 5_000_000, 1).getTransactionResult();
+        assertEquals(Code.SUCCESS, result.getResultCode());
+
+        avmRule.kernel.generateBlock();
+        Address contract = new Address(result.getReturnData());;
+        byte[] data = ABIUtil.encodeMethodArguments("manipulatePutStorageValue");
+        result = avmRule.call(deployer, contract, BigInteger.ZERO, data, 2_000_000, 1).getTransactionResult();
+
+        //TODO: this should be SUCCESS
+        assertEquals(Code.FAILED_REVERT, result.getResultCode());
+    }
+
+    @Test
+    public void testManipulateGetStorageValue() {
+        byte[] jar = avmRule.getDappBytes(StorageManipulationTarget.class, new byte[0]);
+        TransactionResult result = avmRule.deploy(deployer, BigInteger.ZERO, jar, 5_000_000, 1).getTransactionResult();
+        assertEquals(Code.SUCCESS, result.getResultCode());
+
+        avmRule.kernel.generateBlock();
+        Address contract = new Address(result.getReturnData());;
+        byte[] data = ABIUtil.encodeMethodArguments("manipulateGetStorageValue");
+        result = avmRule.call(deployer, contract, BigInteger.ZERO, data, 2_000_000, 1).getTransactionResult();
+
+        //TODO: this should be SUCCESS
+        assertEquals(Code.FAILED_REVERT, result.getResultCode());
+    }
+
+    @Test
+    public void testManipulateStorageInReentrantCall() {
+        byte[] jar = avmRule.getDappBytes(StorageManipulationTarget.class, new byte[0]);
+        TransactionResult result = avmRule.deploy(deployer, BigInteger.ZERO, jar, 5_000_000, 1).getTransactionResult();
+        assertEquals(Code.SUCCESS, result.getResultCode());
+
+        avmRule.kernel.generateBlock();
+        Address contract = new Address(result.getReturnData());;
+        byte[] data = ABIUtil.encodeMethodArguments("manipulateStorageInReentrantCall");
+        result = avmRule.call(deployer, contract, BigInteger.ZERO, data, 2_000_000, 1).getTransactionResult();
+
+        //TODO: this should be SUCCESS
+        assertEquals(Code.FAILED_REVERT, result.getResultCode());
+    }
+}
